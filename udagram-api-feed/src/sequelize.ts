56a2f11267ed2c2cd4c6da88dc997ca1664359f6
Vuongtiger98 <vuonglhgch020498@gmail.com>
2024-10-13 21:31:15 +0700

    end

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,19 +1,14 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+import { Sequelize } from 'sequelize-typescript';
+import { Dialect } from 'sequelize'; // Correct import
+import { config } from './config/config';
 
+const dialect: Dialect = config.dialect as Dialect; // Ensure this is a valid Dialect
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
-  'port': 5432,
-  'dialect': config.dialect,
-  'storage': ':memory:',
-  dialectOptions: {
-    ssl: {
-      require: true, // This will help you. But you will see nwe error
-      rejectUnauthorized: false // This line will fix new error
-    }
-  },
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  dialect: dialect,
+  storage: ':memory:', // Only needed for SQLite
 });
--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -8,11 +8,13 @@ import bodyParser from 'body-parser';
 import {config} from './config/config';
 import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
+
 (async () => {
   await sequelize.addModels(V0_FEED_MODELS);
 
+
   console.debug("Initialize database connection...");
-  await sequelize.sync();
+  //await sequelize.sync();
 
   const app = express();
   const port = process.env.PORT || 8080;
